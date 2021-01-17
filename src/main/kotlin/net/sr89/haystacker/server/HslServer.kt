0c7d701f5a5c5691590033dc5a2806ef51167cd6
Simone <s@sr89.net>
2021-01-17 22:16:33 +0100

    Added TODO for the server

---
+++ src/main/kotlin/net/sr89/haystacker/server/HslServer.kt
@@ -0,0 +1,18 @@
+package net.sr89.haystacker.server
+
+fun startServer(): Unit {
+    TODO(
+        """
+            Start a background process/daemon that listens for commands.
+            
+            Commands:
+            
+            * Perform HSL search on index
+            * Add directory to index
+            * Add directory recursively to index
+            * Remove directory from index
+            * Create new index
+            * Delete index
+        """.trimIndent()
+    )
+}

292e52e6381dc848389d0377735af1a2fe85495c
Simone <s@sr89.net>
2021-02-06 20:30:44 +0100

    Correct status codes

--- haystacker-server/src/main/kotlin/net/sr89/haystacker/server/filter/ExceptionHandlingFilter.kt
+++ haystacker-server/src/main/kotlin/net/sr89/haystacker/server/filter/ExceptionHandlingFilter.kt
@@ -1,10 +1,12 @@
 package net.sr89.haystacker.server.filter
 
 import net.sr89.haystacker.lang.exception.InvalidHslGrammarException
+import net.sr89.haystacker.lang.exception.InvalidSemanticException
 import net.sr89.haystacker.server.api.stringBody
 import org.http4k.core.Filter
 import org.http4k.core.Response
-import org.http4k.core.Status
+import org.http4k.core.Status.Companion.BAD_REQUEST
+import org.http4k.core.Status.Companion.INTERNAL_SERVER_ERROR
 import org.http4k.core.with
 
 fun exceptionHandlingFilter() = Filter { next ->
@@ -12,9 +14,11 @@ fun exceptionHandlingFilter() = Filter { next ->
         try {
             next(it)
         } catch (e: InvalidHslGrammarException) {
-            Response(Status.BAD_REQUEST).with(stringBody of "Unable to parse query ${e.hslQuery}: Invalid HSL query. Refer to the README for a guide: https://github.com/srusso/haystacker")
+            Response(BAD_REQUEST).with(stringBody of "Unable to parse query ${e.hslQuery}: Invalid HSL query. Refer to the README for a guide: https://github.com/srusso/haystacker")
+        } catch (e: InvalidSemanticException) {
+            Response(BAD_REQUEST).with(stringBody of e.message!!)
         } catch (e: Exception) {
-            Response(Status.NOT_FOUND).with(stringBody of e.message!!)
+            Response(INTERNAL_SERVER_ERROR).with(stringBody of e.message!!)
         }
     }
 }

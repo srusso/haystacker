5d2950f7e49c64943004d59eb2badf874f1a571b
Simone <s@sr89.net>
2021-03-25 20:27:19 +0100

    35: REST handler and shell command to interrupt running tasks

--- haystacker-server/src/main/kotlin/net/sr89/haystacker/index/BackgroundIndexingTask.kt
+++ haystacker-server/src/main/kotlin/net/sr89/haystacker/index/BackgroundIndexingTask.kt
@@ -5,6 +5,7 @@ import net.sr89.haystacker.async.task.TaskExecutionState.COMPLETED
 import net.sr89.haystacker.async.task.TaskExecutionState.ERROR
 import net.sr89.haystacker.async.task.TaskExecutionState.INTERRUPTED
 import net.sr89.haystacker.async.task.TaskExecutionState.NOT_STARTED
+import net.sr89.haystacker.async.task.TaskExecutionState.RUNNING
 import net.sr89.haystacker.async.task.TaskStatus
 import net.sr89.haystacker.index.Trigger.COMMAND
 import java.nio.file.Path
@@ -15,7 +16,8 @@ enum class Trigger { FS_UPDATE, COMMAND }
 class BackgroundIndexingTask(
     val trigger: Trigger,
     val indexManager: IndexManager,
-    val directoryToIndex: Path) : BackgroundTask {
+    val directoryToIndex: Path
+) : BackgroundTask {
 
     val latestStatus = AtomicReference(TaskStatus(NOT_STARTED, "Ready to start"))
 
@@ -23,7 +25,9 @@ class BackgroundIndexingTask(
         try {
             indexManager.addNewDirectory(directoryToIndex, latestStatus, trigger == COMMAND)
 
-            latestStatus.set(TaskStatus(COMPLETED, "Indexed $directoryToIndex and subdirectories"))
+            if (latestStatus.get().state == RUNNING) {
+                latestStatus.set(TaskStatus(COMPLETED, "Indexed $directoryToIndex and subdirectories"))
+            }
         } catch (e: Exception) {
             latestStatus.set(TaskStatus(ERROR, e.message!!))
         }
@@ -31,6 +35,7 @@ class BackgroundIndexingTask(
 
     override fun interrupt() {
         // TODO refactor this. it's horrible to interrupt the task by setting the task status
+        println("Interrupting task to index $directoryToIndex")
         latestStatus.set(TaskStatus(INTERRUPTED, "Interrupt command received"))
     }
 

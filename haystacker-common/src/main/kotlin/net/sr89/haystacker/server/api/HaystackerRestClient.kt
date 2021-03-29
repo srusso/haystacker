d52e8d81b097b676aea70da05d86bb9f9a35c6a9
Simone <s@sr89.net>
2021-03-29 13:58:37 +0200

    53: Imports

--- haystacker-common/src/main/kotlin/net/sr89/haystacker/server/api/HaystackerRestClient.kt
+++ haystacker-common/src/main/kotlin/net/sr89/haystacker/server/api/HaystackerRestClient.kt
@@ -2,7 +2,9 @@ package net.sr89.haystacker.server.api
 
 import com.fasterxml.jackson.core.type.TypeReference
 import org.http4k.core.HttpHandler
-import org.http4k.core.Method
+import org.http4k.core.Method.DELETE
+import org.http4k.core.Method.GET
+import org.http4k.core.Method.POST
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status
@@ -13,14 +15,14 @@ import java.time.Duration
 
 class HaystackerRestClient(val baseUrl: String, val underlyingClient: HttpHandler) {
     fun createIndex(indexPath: String): TimedHttpResponse<String> {
-        val request = Request(Method.POST, "$baseUrl/index")
+        val request = Request(POST, "$baseUrl/index")
             .with(indexPathQuery of indexPath)
 
         return executeTimed(request, object: TypeReference<String>() {})
     }
 
     fun indexDirectory(indexPath: String, dirPath: String): TimedHttpResponse<TaskIdResponse> {
-        val request = Request(Method.POST, "$baseUrl/directory")
+        val request = Request(POST, "$baseUrl/directory")
             .with(
                 indexPathQuery of indexPath,
                 directoryQuery of dirPath
@@ -30,7 +32,7 @@ class HaystackerRestClient(val baseUrl: String, val underlyingClient: HttpHandle
     }
 
     fun deindexDirectory(indexPath: String, dirPath: String): TimedHttpResponse<TaskIdResponse> {
-        val request = Request(Method.DELETE, "$baseUrl/directory")
+        val request = Request(DELETE, "$baseUrl/directory")
             .with(
                 indexPathQuery of indexPath,
                 directoryQuery of dirPath
@@ -40,25 +42,25 @@ class HaystackerRestClient(val baseUrl: String, val underlyingClient: HttpHandle
     }
 
     fun taskStatus(taskId: String): TimedHttpResponse<BackgroundTaskStatusResponse> {
-        val request = Request(Method.GET, "$baseUrl/task")
+        val request = Request(GET, "$baseUrl/task")
             .with(taskIdQuery of taskId)
 
         return executeTimed(request, object: TypeReference<BackgroundTaskStatusResponse>() {})
     }
 
     fun taskInterrupt(taskId: String): TimedHttpResponse<TaskInterruptResponse> {
-        val request = Request(Method.POST, "$baseUrl/task/interrupt")
+        val request = Request(POST, "$baseUrl/task/interrupt")
             .with(taskIdQuery of taskId)
 
         return executeTimed(request, object: TypeReference<TaskInterruptResponse>() {})
     }
 
     fun shutdownServer(): TimedHttpResponse<String> {
-        return executeTimed(Request(Method.POST, "$baseUrl/quit"), object: TypeReference<String>() {})
+        return executeTimed(Request(POST, "$baseUrl/quit"), object: TypeReference<String>() {})
     }
 
     fun search(query: String, maxResults: Int, indexPath: String): TimedHttpResponse<SearchResponse> {
-        val request = Request(Method.POST, "$baseUrl/search")
+        val request = Request(POST, "$baseUrl/search")
             .with(
                 hslStringQuery of query,
                 indexPathQuery of indexPath,

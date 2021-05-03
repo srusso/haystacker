3d8cf1735481be8475595a8e1a2502dd79cd9a45
Simone <s@sr89.net>
2021-05-03 18:20:56 +0200

    50: Base search box

--- haystacker-ui/src/main/kotlin/UIStart.kt
+++ haystacker-ui/src/main/kotlin/UIStart.kt
@@ -1,20 +1,42 @@
 package net.sr89.haystacker.ui
 
 import javafx.application.Application
-import javafx.scene.Parent
+import javafx.beans.binding.Bindings
+import javafx.geometry.Pos
 import javafx.scene.Scene
-import javafx.scene.layout.StackPane
-import javafx.scene.text.Text
+import javafx.scene.control.Label
+import javafx.scene.control.TextField
+import javafx.scene.layout.GridPane
+import javafx.scene.layout.HBox
+import javafx.scene.layout.VBox
 import javafx.stage.Stage
 
 
 class UIStart: Application() {
-    private fun createContent(): Parent {
-        return StackPane(Text("Hello World"))
-    }
-
     override fun start(stage: Stage) {
-        stage.scene = Scene(createContent(), 300.0, 300.0)
+        val startLabel = Label("Search")
+        val searchTestBox = TextField()
+        searchTestBox.textProperty().bind(
+            Bindings.format("HSL query here")
+        )
+
+        val gp = GridPane()
+        gp.add(startLabel, 0, 0)
+        gp.add(searchTestBox, 1, 0)
+        gp.hgap = 10.0
+        gp.vgap = 10.0
+
+        val hbox = HBox(gp)
+        hbox.alignment = Pos.TOP_CENTER
+
+        val vbox = VBox(hbox)
+        vbox.alignment = Pos.TOP_CENTER
+
+        val scene = Scene(vbox, 480.0, 320.0)
+
+        stage.scene = scene
+
+        stage.title = "Haystacker"
         stage.show()
     }
 
